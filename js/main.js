e8a494e59b7a0f30ec99b33cc14f19ac949ed337
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-01-06 02:30:34 -0300

    Change of values on variables

--- js/main.js
+++ js/main.js
@@ -356,7 +356,7 @@ let bothPriceMultiplier = 1.7;
 let imageServicePrice = 30;
 let videoServicePrice = 50; // for 15 seconds of video
 let panoramicServicePrice = 70;
-let interactiveServicePrice = 300;
+let interactiveServicePrice = 250;
 
 // ----------------------------------- Internal Variables
 let totalProjectPrice = 0;
